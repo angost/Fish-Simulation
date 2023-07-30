1ade8d907ee60327c99d575e353d0581d2b612a4
Angelika Ostrowska <01178572@pw.edu.pl>
2023-07-30 22:22:42 +0200

    Testing

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import pygame
 from random import randint, choice
 from Fish import Fish
 
-def fish_setup():
+def fish_setup(nr_of_fish):
     fish = []
     fish_names = ("Losos", "Karp", "Pstrag", "Dorsz", "Rozdymka", "Miecznik", "Nemo")
 
-    for i in range(10):
+    for fish_index in range(nr_of_fish):
         fish.append(Fish
                     (choice(fish_names),
                     randint(1,300),
@@ -16,6 +16,7 @@ def fish_setup():
                     randint(5,100)))
     return fish
 
+
 def main():
     WIDTH = 800
     HEIGHT = 600
@@ -23,11 +24,17 @@ def main():
     screen = pygame.display.set_mode([WIDTH, HEIGHT])
     clock = pygame.time.Clock()
 
+    fish = fish_setup(10)
+    for f in fish:
+        print(f)
+
     main_loop = True
     while main_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 main_loop = False
+
+        fish[0].swim()
 
         screen.fill((42, 108, 212))
         pygame.display.flip()
