0f731ce1fc8bb8d85747d8b678b19cb048235567
Angelika Ostrowska <01178572@pw.edu.pl>
2023-08-02 17:10:18 +0200

    Started to implement mouse click actions

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def fish_setup(nr_of_fish):
     return fish
 
 
+
 def main():
     # WIDTH = 800
     # HEIGHT = 600
@@ -39,20 +40,37 @@ def main():
     for fish in all_fish:
         print(fish)
 
+    follow_mouse = False
     main_loop = True
     while main_loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 main_loop = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if not follow_mouse and pygame.mouse.get_pressed()[0]:
+                    follow_mouse = True
+                    print("Follow mouse")
+                elif pygame.mouse.get_pressed()[2]:
+                    print("Spawn food")
+                pos = pygame.mouse.get_pos()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if follow_mouse and not pygame.mouse.get_pressed()[0]:
+                    follow_mouse = False
+                    print("Stop following")
 
+
+        # Semi-transparent circles
         screen.fill((42, 108, 212))
         transparent_surface.fill((42, 108, 212))
         for fish in all_fish:
             pygame.draw.circle(transparent_surface, tuple(list(fish.color) + [190]), fish.pos, max(fish.size)/2)
         screen.blit(transparent_surface, (0, 0))
+        # Fish imgs, functionality
         for fish in all_fish:
             screen.blit(fish.img, fish.img_pos)
             fish.swim(all_fish)
+
+
 
         pygame.display.flip()
         clock.tick(45)
